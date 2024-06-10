75576b90a9ea39a2bc6fed227d07de877c760ea9
Indra Arianggi Suryaatmaja <indraarianggi@gmail.com>
2024-06-10 20:23:23 +0700

    header navigation

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "IndraBlog",
+  url: "https://example.com",
+  description: "Nextjs blog using velite, tailwind and shadcn",
+  author: "Indra Arianggi",
+  links: {
+    twitter: "https://twitter.com/indraarianggi",
+    github: "https://github.com/indraarianggi",
+    linkedin: "https://linkedin.com/in/indraarianggi",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
